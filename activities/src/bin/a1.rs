80dc16fcaed5561b0722b5879bf085570ae9fcb9
ChihYing <spacetime0311@gmail.com>
2023-07-17 17:30:48 +0800

    feat: Complete a1

--- activities/src/bin/a1.rs
+++ activities/src/bin/a1.rs
@@ -8,4 +8,17 @@
 // * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+// * Use a function to display your first name
+fn first_name() {
+    println!("Chihying");
+}
+
+// * Use a function to display your last name
+fn last_name() {
+    println!("Yen");
+}
+
+fn main() {
+    first_name();
+    last_name();
+}
