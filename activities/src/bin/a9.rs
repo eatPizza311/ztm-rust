c938c10a529d98edaa228677ef258a5af6657feb
Chih Ying <spacetime0311@gmail.com>
2023-07-19 17:28:27 +0800

    feat: Complete a9

--- activities/src/bin/a9.rs
+++ activities/src/bin/a9.rs
@@ -9,4 +9,21 @@
 // * Destructure the return value into two variables
 // * Use an if..else if..else block to determine what to print
 
-fn main() {}
+// Use a function that returns a tuple
+fn cartesian() -> (i32, i32) {
+    (10, 5)
+}
+
+fn main() {
+    // Destructure the return value into two variables
+    let (x, y) = cartesian();
+
+    // Use an if..else if..else block to determine what to print
+    if y > 5 {
+        println!(">5")
+    } else if y < 5 {
+        println!("<5")
+    } else {
+        println!("=5")
+    }
+}
