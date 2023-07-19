158eb4d63f00d1d11672d6d606b597132d7405dc
ChihYing <spacetime0311@gmail.com>
2023-07-19 21:37:22 +0800

    feat: Complete a10

--- activities/src/bin/a10.rs
+++ activities/src/bin/a10.rs
@@ -12,4 +12,18 @@
 // * Use a match expression to determine which message
 //   to print
 
-fn main() {}
+// Use a function to print the messages
+fn print_message(is_big: bool) {
+    // Use a match expression to determine which message
+    match is_big {
+        true => println!("its big"),
+        false => println!("its small"),
+    }
+}
+
+fn main() {
+    let number: i32 = 100;
+    // Use a boolean variable set to the result of an if..else expression to store whether the value is
+    let is_big: bool = number > 100;
+    print_message(is_big)
+}
