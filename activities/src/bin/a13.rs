3c13765f86313256291316479a44531cc8e016de
ChihYing <spacetime0311@gmail.com>
2023-07-22 21:15:40 +0800

    feat: Complete a13

--- activities/src/bin/a13.rs
+++ activities/src/bin/a13.rs
@@ -10,4 +10,18 @@
 // * Determine whether to print the number or print "thirty" inside the loop
 // * Use the .len() function to print the number of elements in a vector
 
-fn main() {}
+fn main() {
+    // Use a vector to store 4 numbers
+    let my_vec: Vec<i32> = vec![10, 20, 30, 40];
+
+    // Iterate through the vector using a for..in loop
+    for num in &my_vec {
+        // Determine whether to print the number or print "thirty" inside the loop
+        match num {
+            30 => println!("thirty"),
+            _ => println!("{:?}", num),
+        }
+    }
+    // Use the .len() function to print the number of elements in a vector
+    println!("The length of vector is {:?}", my_vec.len());
+}
