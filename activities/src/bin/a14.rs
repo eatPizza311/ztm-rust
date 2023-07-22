cb541b2499c12e3c662b29b294230b4857ef50f9
ChihYing <spacetime0311@gmail.com>
2023-07-22 21:37:09 +0800

    feat: Complete a14

--- activities/src/bin/a14.rs
+++ activities/src/bin/a14.rs
@@ -11,4 +11,45 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+// Use a struct for a persons age, name, and favorite color
+struct Person {
+    age: i32,
+    // The color and name should be stored as a String
+    fav_color: String,
+    name: String,
+}
+
+fn print_fav(name: &str, color: &str) {
+    println!("{:?} favorite color is {:?}", name, color)
+}
+
+fn main() {
+    // Create and store at least 3 people in a vector
+    let people: Vec<Person> = vec![
+        Person {
+            age: 15,
+            fav_color: "Blue".to_owned(),
+            name: "John".to_owned(),
+        },
+        Person {
+            age: 10,
+            fav_color: String::from("Pink"),
+            name: String::from("Mary"),
+        },
+        Person {
+            age: 6,
+            fav_color: "Green".to_owned(),
+            name: String::from("David"),
+        },
+    ];
+
+    // Iterate through the vector using a for..in loop
+    for person in people {
+        // Use an if expression to determine which person's info should be printed
+        // Print out the name and favorite colors of people aged 10 and under
+        if person.age <= 10 {
+            // The name and colors should be printed using a function
+            print_fav(&person.name, &person.fav_color);
+        }
+    }
+}
