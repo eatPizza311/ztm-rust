ba898d5327cc61f7a7e90d5accd4a766a69e6376
ChihYing <spacetime0311@gmail.com>
2023-07-24 21:46:27 +0800

    feat: Complete a17

--- activities/src/bin/a17.rs
+++ activities/src/bin/a17.rs
@@ -12,4 +12,17 @@
 //   to uppercase and lowercase
 //   * Try searching for: to_uppercase, to_lowercase
 
-fn main() {}
+fn main() {
+    let my_string_lower = "hello";
+    println!(
+        "Lower case string {:?} can be convert to upper case using .to_uppercase {:?}",
+        my_string_lower,
+        my_string_lower.to_uppercase()
+    );
+    let my_string_upper = "HELLO";
+    println!(
+        "Upper case string {:?} can be convert to lower case using .to_lowercase {:?}",
+        my_string_upper,
+        my_string_upper.to_lowercase()
+    )
+}
