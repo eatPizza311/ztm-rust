14793813af015a34658049aca3efb39603b104f8
ChihYing <spacetime0311@gmail.com>
2023-07-24 16:45:23 +0800

    feat: Complete a16

--- activities/src/bin/a16.rs
+++ activities/src/bin/a16.rs
@@ -8,4 +8,28 @@
 // * Use a struct containing the student's name and locker assignment
 // * The locker assignment should use an Option<i32>
 
-fn main() {}
+// Use a struct containing the student's name and locker assignment
+// The locker assignment should use an Option<i32>
+struct Locker {
+    name: String,
+    locker: Option<i32>,
+}
+fn main() {
+    let lockers = vec![
+        Locker {
+            name: "Amy".to_owned(),
+            locker: Some(32),
+        },
+        Locker {
+            name: String::from("Geoge"),
+            locker: None,
+        },
+    ];
+    for lk in lockers {
+        println!("Student: {:?}", lk.name);
+        match lk.locker {
+            Some(num) => println!("Locker number is {:?}", num),
+            None => println!("No locker assigned!"),
+        }
+    }
+}
