643aaa85673c2fb28e372eca5559947e784d1f55
Chih Ying <spacetime0311@gmail.com>
2023-07-25 15:25:14 +0800

    feat: Complete a19

--- activities/src/bin/a19.rs
+++ activities/src/bin/a19.rs
@@ -12,5 +12,24 @@
 //
 // Notes:
 // * Use a HashMap for the furniture store stock
+use std::collections::HashMap;
 
-fn main() {}
+fn main() {
+    let mut stocks = HashMap::new();
+    stocks.insert("Chairs", 5);
+    stocks.insert("Beds", 3);
+    stocks.insert("Tables", 2);
+    stocks.insert("Couches", 0);
+
+    let mut total = 0;
+    for (item, qty) in stocks.iter() {
+        total += qty;
+        let stock_count = if qty == &0 {
+            "out of stock".to_owned()
+        } else {
+            format!("{:?}", qty)
+        };
+        println!("item={:?}, stock={:?}", item, stock_count);
+    }
+    println!("The total stock={:?}", total);
+}
