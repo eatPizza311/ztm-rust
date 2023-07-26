af929f1c4a313c2929ee25a0a90e9c62aa92cde0
Chih Ying <spacetime0311@gmail.com>
2023-07-26 12:59:33 +0800

    feat: Complete a22

--- activities/src/bin/a22.rs
+++ activities/src/bin/a22.rs
@@ -23,12 +23,69 @@ fn clamp(n: i32, lower: i32, upper: i32) -> i32 {
 
 /// Divides a and b.
 fn div(a: i32, b: i32) -> Option<i32> {
-    Some(a / b)
+    if b == 0 {
+        return None;
+    } else {
+        Some(a / b)
+    }
 }
 
 /// Takes two strings and places them immediately one after another.
 fn concat(first: &str, second: &str) -> String {
-    format!("{} {}", first, second)
+    format!("{}{}", first, second)
 }
 
 fn main() {}
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+
+    #[test]
+    fn test_clamp_lower() {
+        let result = clamp(10, 15, 20);
+        let expected = 15;
+        assert_eq!(
+            result, expected,
+            "Value less than lower bound should be equal to lower bound."
+        );
+    }
+
+    #[test]
+    fn test_clamp_upper() {
+        let result = clamp(25, 15, 20);
+        let expected = 20;
+        assert_eq!(
+            result, expected,
+            "Value larger than upper bound should be equal to upper bound."
+        );
+    }
+
+    #[test]
+    fn test_clamp_inside() {
+        let result = clamp(15, 15, 20);
+        let expected = 15;
+        assert_eq!(result, expected, "Value inside the range should be itself.");
+    }
+
+    #[test]
+    fn test_10_div_2() {
+        let result = div(10, 2);
+        let expected = Some(5);
+        assert_eq!(result, expected, "10/2 should be 5.")
+    }
+
+    #[test]
+    fn test_1_div_0() {
+        let result = div(1, 0);
+        let expected = None;
+        assert_eq!(result, expected, "should catch divide by 0.")
+    }
+
+    #[test]
+    fn test_concat() {
+        let result = concat("me", "you");
+        let expected = "meyou".to_owned();
+        assert_eq!(result, expected, "should be meyou, got {result} instead.")
+    }
+}
