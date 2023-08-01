a3c00be1c19ce05f78c1c9912e4d30f439a860ea
ChihYing <spacetime0311@gmail.com>
2023-08-01 21:28:29 +0800

    feat: Complete a25

--- activities/src/bin/a25.rs
+++ activities/src/bin/a25.rs
@@ -12,4 +12,54 @@
 // * Use a single function to print out the perimeter of the shapes
 //   * The function must utilize impl trait as a function parameter
 
-fn main() {}
+// Use a trait to declare a perimeter calculation function
+trait Perimeter {
+    fn calc_perimeter(&self) -> i32;
+}
+
+struct Square {
+    length: i32,
+}
+
+impl Perimeter for Square {
+    fn calc_perimeter(&self) -> i32 {
+        self.length * 4
+    }
+}
+
+struct Triangle {
+    side1: i32,
+    side2: i32,
+    side3: i32,
+}
+
+impl Triangle {
+    fn new(side1: i32, side2: i32, side3: i32) -> Option<Triangle> {
+        // Check if the side lengths fulfill the triangle property
+        if side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1 {
+            Some(Triangle {
+                side1,
+                side2,
+                side3,
+            })
+        } else {
+            None
+        }
+    }
+}
+
+impl Perimeter for Triangle {
+    fn calc_perimeter(&self) -> i32 {
+        self.side1 + self.side2 + self.side3
+    }
+}
+
+// Use a single function to print out the perimeter of the shapes
+//   The function must utilize impl trait as a function parameter
+fn print_perimeter(shape: impl Perimeter) {
+    println!("{:?}", shape.calc_perimeter());
+}
+fn main() {
+    print_perimeter(Square { length: 3 });
+    print_perimeter(Triangle::new(3, 4, 5).expect("This is a valid triangle!"));
+}
