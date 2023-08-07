4064927a60d935fc77d53a34fa864e255ed25952
ChihYing <spacetime0311@gmail.com>
2023-08-07 10:19:45 +0800

    feat: Complete a35

--- activities/src/bin/a35.rs
+++ activities/src/bin/a35.rs
@@ -54,4 +54,47 @@ enum Tile {
     Wood,
 }
 
-fn main() {}
+fn print_tile(tile: Tile) {
+    use Tile::*;
+    // * Bricks:
+    //   * Colored bricks should print "The brick color is [color]"
+    //   * Other bricks should print "[Bricktype] brick"
+    match tile {
+        Brick(brick @ BrickStyle::Gray | brick @ BrickStyle::Red) => {
+            println!("The brick color is {brick:?}")
+        }
+        Brick(other) => println!("{other:?} brick"),
+        // * Grass, Dirt, and Sand should all print "Ground tile"
+        Grass | Dirt | Sand => println!("Ground tile"),
+        // * Water:
+        //   * Pressure levels 10 and over should print "High water pressure!"
+        //   * Pressure levels under 10 should print "Water pressure level: [Pressure]"
+        Water(pressure) if pressure.0 >= 10 => println!("High water pressure!"),
+        Water(pressure) if pressure.0 < 10 => println!("Water pressure level: {}", pressure.0),
+
+        // * Treasure Chests:
+        //   * If the treasure is Gold and the amount is at least 100, print "Lots of gold!"
+        Treasure(TreasureChest {
+            amount,
+            content: TreasureItem::Gold,
+        }) if amount >= 100 => println!("Lots of gold!"),
+        // * Everything else shoud not print any messages
+        _ => (),
+    }
+}
+fn main() {
+    let tile1 = Tile::Brick(BrickStyle::Gray);
+    print_tile(tile1);
+
+    let tile2 = Tile::Grass;
+    print_tile(tile2);
+
+    let tile3 = Tile::Treasure(TreasureChest {
+        content: TreasureItem::Gold,
+        amount: 123,
+    });
+    print_tile(tile3);
+
+    let tile4 = Tile::Water(Pressure(12));
+    print_tile(tile4);
+}
