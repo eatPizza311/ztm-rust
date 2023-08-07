3f4964087b46b5e10afd0e4d4a72e8a024fd200f
ChihYing <spacetime0311@gmail.com>
2023-08-07 13:58:35 +0800

    feat: Complete a36

--- activities/src/bin/a36.rs
+++ activities/src/bin/a36.rs
@@ -17,4 +17,12 @@ fn data() -> &'static [u64] {
 fn main() {
     // `stream` is an iterator of Option<&[u64]>
     let mut stream = data().chunks(2);
+    for pair in stream {
+        match pair {
+            [one, two] => println!("{one} + {two} = {}", one + two),
+            [single] => println!("Unpaired value: {single}"),
+            [] => println!("Data stream complete"),
+            [..] => unreachable!("chunk size should be at most 2"),
+        }
+    }
 }
