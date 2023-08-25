4787263e80bce2fc8877ab829fdff45ed8cd7318
ChihYing <spacetime0311@gmail.com>
2023-08-25 15:52:32 +0800

    feat: Service implementation for new clip

--- clipstash/src/lib/data/model.rs
+++ clipstash/src/lib/data/model.rs
@@ -1,4 +1,5 @@
 use crate::data::DbId;
+use crate::domain::clip::field::Content;
 use crate::{ClipError, ShortCode, Time};
 use chrono::{NaiveDateTime, Utc};
 use std::convert::TryFrom;
@@ -66,9 +67,23 @@ pub struct NewClip {
     pub(in crate::data) shortcode: String,
     pub(in crate::data) content: String,
     pub(in crate::data) title: Option<String>,
-    pub(in crate::data) posted: i16,
-    pub(in crate::data) expires: Option<NaiveDateTime>,
+    pub(in crate::data) posted: i64,
+    pub(in crate::data) expires: Option<i64>,
     pub(in crate::data) password: Option<String>,
+}
+
+impl From<crate::service::ask::NewClip> for NewClip {
+    fn from(req: crate::service::ask::NewClip) -> Self {
+        Self {
+            clip_id: DbId::new().into(),
+            content: req.content.into_inner(),
+            title: req.title.into_inner(),
+            expires: req.expires.into_inner().map(|time| time.timestamp()),
+            password: req.password.into_inner(),
+            shortcode: ShortCode::default().into(),
+            posted: Utc::now().timestamp(),
+        }
+    }
 }
 
 pub struct UpdateClip {
