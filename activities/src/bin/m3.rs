544934d49c7ad26543d055964503072715c22e2d
Chih Ying <spacetime0311@gmail.com>
2023-08-29 17:17:59 +0800

    feat: Complete m3

--- activities/src/bin/m3.rs
+++ activities/src/bin/m3.rs
@@ -5,4 +5,24 @@
 //   * The macro must be able to accept multiple key/value pairs.
 //   * Print out the generated hashmap using the `dbg!` macro to ensure it works.
 
-fn main() {}
+
+macro_rules! hashmap {
+    (
+        $( $key:expr => $value:expr ),+
+        $(,)?
+    ) => {{
+        let mut h = ::std::collections::HashMap::new();
+        $(
+            h.insert($key, $value);
+        )+
+        h
+    }};
+}
+fn main() {
+    let hashmap = hashmap!{
+        1 => "a",
+        2 => "b",
+        3 => "c",
+    };
+    dbg!(hashmap);
+}
