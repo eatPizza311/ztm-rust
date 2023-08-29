a4d7a23f3fdb42ed23be5f018afef4b448da2224
Chih Ying <spacetime0311@gmail.com>
2023-08-29 16:51:15 +0800

    feat: demo-macro-repetitions

--- activities/src/bin/demo-macro-repetitions.rs
+++ activities/src/bin/demo-macro-repetitions.rs
@@ -1,1 +1,26 @@
-fn main() {}
+
+macro_rules! myvec {
+    (
+        $( $element:expr ),+
+        $(,)?
+    ) => {{
+        let mut v = Vec::new();
+        $(
+            v.push($element);
+        )+
+        v
+    }};
+}
+
+fn main() {
+    let v1 = myvec![1, 2, 3, 4];
+    let v2 = {
+        let mut v = Vec::new();
+        v.push(1);
+        v.push(2);
+        v.push(3);
+        v.push(4);
+        v
+    };
+    assert_eq!(v1, v2)
+}
