330847ff577439bc906627c7c99e19aef16ad169
Chih Ying <spacetime0311@gmail.com>
2023-08-30 13:09:54 +0800

    feat: demo-macro-recursive

--- activities/src/bin/demo-macro-recursive.rs
+++ activities/src/bin/demo-macro-recursive.rs
@@ -1,4 +1,17 @@
 macro_rules! html {
+    //base case
+    ($w:expr,) => {}; // this is the "&mut data," part
+
+    // recursive
+    // for the most inner case like "Demo title"
+    ($w:expr, $e:tt) => (write!($w, "{}", $e));
+    // for the case with [] in it
+    ($w:expr, $tag:ident [ $($inner:tt)* ] $($rest:tt)* ) => {{
+        write!($w, "<{}>", stringify!($tag));
+        html!($w, $($inner)*);
+        write!($w, "</{}>", stringify!($tag));
+        html!($w, $($rest)*);
+    }};
 }
 
 #[allow(unused_must_use)]
